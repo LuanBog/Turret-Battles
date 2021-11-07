f2d7b3f2f82aaf13b1ada3790cdfea2d7809e6a0
LuanBog <mynameiscutepanda@gmail.com>
2021-11-07 13:27:11 +0800

    Added turret and shooting

---
+++ Turret Battles/Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        transform.position += transform.right * moveSpeed * Time.deltaTime;
+    }
+
+    void FixedUpdate() {
+        // rb.AddForce(transform.up * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
---
+++ Turret Battles/Assets/Scripts/Turret.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    public float rotationSpeed;
+    public GameObject bullet;
+    public Transform shootPoint;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        // Movement
+
+        Vector3 rotation = new Vector3(0f, 0f, rotationSpeed * Time.deltaTime);
+
+        transform.Rotate(rotation);
+
+        // Shooting
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Shoot();
+        }
+    }
+
+    void Shoot() {
+        GameObject bulletClone = (GameObject) Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+    }
+}
